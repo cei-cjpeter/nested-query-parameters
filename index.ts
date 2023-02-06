b72f55326b69867212ee4ae550a97cd06ff0214e
cei-cjpeter <63299912+cei-cjpeter@users.noreply.github.com>
2023-02-06 19:58:12 +0530

    add support for arrays

--- index.ts
+++ index.ts
@@ -5,13 +5,14 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-type ParamValue = string | { [key: string]: ParamValue };
+type ParamValue = string | string[] | { [key: string]: ParamValue };
 console.log(
   convertToQueryParams({
     test: 'test',
     testNested: {
       one: 'one',
       two: 'two',
+      three: ['test1', 'test2'],
       level2: {
         twoOne: 'twoOne',
       },
@@ -33,9 +34,13 @@ function convertToQueryParams(
       if (typeof value === 'string') {
         return { [paramKey]: value };
       }
+      if (Array.isArray(value)) {
+        return value.map((v, i) => ({ [`${paramKey}[${i}]`]: v }));
+      }
 
       return convertToQueryParams(value, paramKey);
     })
+    .flat()
     .reduce((a, b) => {
       Object.assign(a, b);
       return a;
